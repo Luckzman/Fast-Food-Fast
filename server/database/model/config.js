10d34d7dfd4eebd6d7ff41da5543ac6e18ea5218
luckzman <lumpsey@gmail.com>
2018-09-26 17:00:12 +0100

    setup test for database api

--- server/database/model/config.js
+++ server/database/model/config.js
@@ -1,16 +1,35 @@
 import pg from 'pg';
 
-const env = require('dotenv');
+require('dotenv').config();
 
-env.config();
+let connString;
 
-const dbConnection = new pg.Pool({
+const devConnString = {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-});
+};
+
+const devConnStringTest = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_TEST,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+
+if (process.env.NODE_ENV === 'test') {
+  connString = devConnStringTest;
+}
+if (process.env.NODE_ENV === 'production') {
+  connString = process.env.DATABASE_URL;
+}
+connString = devConnString;
+
+
+const dbConnection = new pg.Pool(connString);
 
 dbConnection.on('connect', () => {
   console.log('database connected successfully');
---
+++ server/database/model/db_test.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS db_fastfoodfast_test;
+CREATE DATABASE db_fastfoodfast_test;
+
+\c db_fastfoodfast_test;
+
+CREATE TABLE users(
+    id UUID PRIMARY KEY,
+    firstname VARCHAR NOT NULL,
+    lastname VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    phone VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    user_status VARCHAR DEFAULT 'regular',
+    created_date TIMESTAMPTZ,
+    modified_date TIMESTAMPTZ
+);
+
+CREATE TABLE orders(
+    id UUID PRIMARY KEY,
+    quantity_ordered INT NOT NULL,
+    order_status VARCHAR DEFAULT 'pending',
+    created_date TIMESTAMPTZ,
+    modified_date TIMESTAMPTZ,
+    user_id UUID NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE food_menus(
+    id UUID PRIMARY KEY,
+    food_name VARCHAR NOT NULL,
+    description TEXT NOT NULL,
+    category VARCHAR NOT NULL,
+    price VARCHAR NOT NULL,
+    image VARCHAR NOT NULL,
+    created_date TIMESTAMPTZ,
+    modified_date TIMESTAMPTZ,
+    order_id UUID NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE
+);
+
