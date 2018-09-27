bd116ea86de88222d23cbca8888f810c73463479
luckzman <lumpsey@gmail.com>
2018-09-27 08:38:28 +0100

    ensure create food and get food controller works

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createMenu, getMenu } from '../database/controller/menu';
+import { authCheck } from '../database/middleware/helpers';
 
 const authRouter = express.Router();
 
-authRouter.post('/', createMenu);
+authRouter.post('/', authCheck, createMenu);
 authRouter.get('/', getMenu);
 
 export default authRouter;
