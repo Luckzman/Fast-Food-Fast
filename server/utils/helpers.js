607e863685c270bd7b032eb975b8029cb199b13f
luckzman <lumpsey@gmail.com>
2018-10-01 21:14:00 +0100

    remove unnecessary comment

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,13 +1,4 @@
 import multer from 'multer';
-// import img from './../../menu_img'
-// const storage = multer.diskStorage({
-//   dest: (req, file, cb) => {
-//     cb(null, './menu_img/');
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, `${new Date().toISOString()}${file.originalname}`);
-//   },
-// });
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'image/menu/');
